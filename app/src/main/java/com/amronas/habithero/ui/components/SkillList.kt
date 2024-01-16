e2178692dbe2516a010d73b9dd82f92a69442d0b
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-01-16 15:35:10 +0100

    Remove imports

--- app/src/main/java/com/amronas/habithero/ui/components/SkillList.kt
+++ app/src/main/java/com/amronas/habithero/ui/components/SkillList.kt
@@ -1,25 +1,15 @@
 package com.amronas.habithero.ui.components
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import com.amronas.habithero.data.Skill
 import com.amronas.habithero.data.TestDataProvider
 import com.amronas.habithero.ui.screens.HomeScreen
